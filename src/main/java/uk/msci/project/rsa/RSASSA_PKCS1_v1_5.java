c8aae82e6dbf1ccd35b91b98b31c75a10b041189
Jude Asare <jude.asare.2020@live.rhul.ac.uk>
2023-11-10 11:01:37 +0000

    Refactor the key components (modulus, exponent), modulus bit length and and equivalent octct count (emLen) into fields of the RSASSA_PKCS1_v1_5 class

--- src/main/java/uk/msci/project/rsa/RSASSA_PKCS1_v1_5.java
+++ src/main/java/uk/msci/project/rsa/RSASSA_PKCS1_v1_5.java
@@ -1,5 +1,7 @@
 package uk.msci.project.rsa;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
 import java.util.Arrays;
 import java.util.zip.DataFormatException;
 
@@ -9,6 +11,26 @@ import java.util.zip.DataFormatException;
  * v1.5 specification.
  */
 public class RSASSA_PKCS1_v1_5 {
+
+  /**
+   * The exponent part of the RSA key.
+   */
+  private BigInteger exponent;
+
+  /**
+   * The modulus part of the RSA key.
+   */
+  private BigInteger modulus;
+
+  /**
+   * The bit length of the modulus minus one.
+   */
+  private int emBits;
+
+  /**
+   * The maximum message length in bytes.
+   */
+  private int emLen;
 
   /**
    * The RSA key containing the exponent and modulus.
@@ -25,22 +47,25 @@ public class RSASSA_PKCS1_v1_5 {
    */
   public RSASSA_PKCS1_v1_5(Key key) {
     this.key = key;
+    this.exponent = this.key.getExponent();
+    this.modulus = this.key.getModulus();
+    // emBits is the bit length of the modulus n, minus one.
+    this.emBits = modulus.bitLength() - 1;
+    // emLen is the maximum message length in bytes.
+    this.emLen = (this.emBits + 7) / 8; // Convert bits to bytes and round up if necessary.
   }
 
 
   public byte[] EMSA_PKCS1_v1_5_ENCODE(byte[] M) throws DataFormatException {
 
-    int emBits = this.key.getModulus().bitLength() - 1;
-    int emLen = (emBits + 7) / 8;
-
     // Prepare padding string PS consisting of padding bytes (0xFF).
     int psLength =
-        emLen - M.length - 3; // Subtracting the prefix (0x00 || 0x01) and postfix (0x00) lengths
+        this.emLen - M.length - 3; // Subtracting the prefix (0x00 || 0x01) and postfix (0x00) lengths
     byte[] PS = new byte[psLength];
     Arrays.fill(PS, (byte) 0xFF);
 
     // Concatenate PS, the DigestInfo, and other padding to form the encoded message EM.
-    byte[] EM = new byte[emLen];
+    byte[] EM = new byte[this.emLen];
     int offset = 0;
     EM[offset++] = 0x00; // Initial 0x00
     EM[offset++] = 0x01; // Block type 0x01 for PKCS signatures
