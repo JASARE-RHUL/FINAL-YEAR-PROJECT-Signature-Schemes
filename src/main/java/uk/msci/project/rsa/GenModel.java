88e8bc4418ef5308de401156da2bd1760b97229f
Jude Asare <jude.asare.2020@live.rhul.ac.uk>
2023-11-21 18:10:14 +0000

    Create a class GenModel, part of the Model component specific to the RSA key generation process that encapsulates the data and the logic required to key track of a user initiated key generation process

---
+++ src/main/java/uk/msci/project/rsa/GenModel.java
@@ -0,0 +1,20 @@
+package uk.msci.project.rsa;
+
+import java.io.IOException;
+
+/**
+ * This class is part of the Model component specific to the RSA key generation process. It
+ * encapsulates the data and the logic required to key track of a user initiated key generation
+ * process
+ */
+public class GenModel {
+
+  /**
+   * Constructor for GenModel. This initializes the model which will be bound to the runtime
+   * behavior of the signature program. At the point of launch, the model does not have any state
+   * until it is initiated by the user.
+   */
+  public GenModel() {
+  }
+
+}
---
+++ src/test/java/uk/msci/project/test/GenModelTest.java
@@ -0,0 +1,42 @@
+package uk.msci.project.test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import uk.msci.project.rsa.GenModel;
+import uk.msci.project.rsa.Key;
+import uk.msci.project.rsa.PrivateKey;
+
+
+public class GenModelTest {
+
+  private GenModel genModel;
+
+
+  @BeforeEach
+  // Before each test is run, clear any created key files.
+  public void setup() {
+    genModel = new GenModel();
+
+  }
+
+  @Test
+  void testInitialization() {
+    assertNotNull("GenModel should initialise an object", genModel);
+  }
+
+
+}
+
