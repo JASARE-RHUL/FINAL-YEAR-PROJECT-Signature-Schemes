3cdf3ac2e2f98f7af340724567ee47b91a43e4bc
Jude Asare <jude.asare.2020@live.rhul.ac.uk>
2023-12-01 12:19:47 +0000

    Modify the application to launch from a new class RunApplication to prevent failure to load JavaFX at run time

---
+++ application/controllers/uk/msci/project/rsa/RunApplication.java
@@ -0,0 +1,11 @@
+package uk.msci.project.rsa;
+
+/**
+ * This class serves as the entry point to launch the LaunchMainMenu application.
+ */
+public class RunApplication {
+
+  public static void main(String[] args) {
+    LaunchMainMenu.main(args);
+  }
+}
