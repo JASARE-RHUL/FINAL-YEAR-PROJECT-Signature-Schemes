090c1d0c1f67b28162fcb6864de10ff738429b7a
Jude Asare <jude.asare.2020@live.rhul.ac.uk>
2024-02-14 10:36:17 +0000

    Update DigestFactory to return message digests for recently addded extendable output hash functions

--- application/modules/signatures/models/uk/msci/project/rsa/DigestFactory.java
+++ application/modules/signatures/models/uk/msci/project/rsa/DigestFactory.java
@@ -2,32 +2,70 @@ package uk.msci.project.rsa;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import uk.msci.project.rsa.exceptions.InvalidDigestException;
+import java.security.NoSuchProviderException;
+import java.security.Security;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 /**
- * The  class provides a method to create MessageDigest instances based on a given DigestType.
+ * The DigestFactory class provides a method to create MessageDigest instances based on a given
+ * DigestType.
  */
 public class DigestFactory {
 
   /**
+   * Indicates whether the current hash function is fixed-size or not.
+   */
+  private static boolean isFixedHash;
+
+  /**
    * Creates and returns a MessageDigest instance corresponding to the specified DigestType.
-   * Currently supports SHA-256 and SHA-512 hash functions.
    *
    * @param digestType The type of the digest to be created, defined by the DigestType enum.
    * @return A MessageDigest instance corresponding to the specified type.
-   * @throws InvalidDigestException   If the specified digest type is not supported or invalid.
    * @throws NoSuchAlgorithmException If the algorithm for the requested digest type is not
    *                                  available.
+   * @throws NoSuchProviderException  If the BouncyCastle provider is not available when trying to
+   *                                  create a SHAKE message digest.
    */
   public static MessageDigest getMessageDigest(DigestType digestType)
-      throws InvalidDigestException, NoSuchAlgorithmException {
+      throws NoSuchAlgorithmException, NoSuchProviderException {
     switch (digestType) {
-      case SHA_256:
+      case SHA_256 -> {
+        isFixedHash = true;
         return MessageDigest.getInstance("SHA-256");
-      case SHA_512:
+      }
+      case MGF_1_SHA_256 -> {
+        isFixedHash = false;
+        return MessageDigest.getInstance("SHA-256");
+      }
+      case SHA_512 -> {
+        isFixedHash = true;
         return MessageDigest.getInstance("SHA-512");
-      default:
-        throw new InvalidDigestException("Invalid hash function type");
+      }
+      case MGF_1_SHA_512 -> {
+        isFixedHash = false;
+        return MessageDigest.getInstance("SHA-512");
+      }
+      case SHAKE_128 -> {
+        isFixedHash = false;
+        Security.addProvider(new BouncyCastleProvider());
+        return MessageDigest.getInstance("SHAKE128", BouncyCastleProvider.PROVIDER_NAME);
+      }
+      case SHAKE_256 -> {
+        isFixedHash = false;
+        Security.addProvider(new BouncyCastleProvider());
+        return MessageDigest.getInstance("SHAKE256", BouncyCastleProvider.PROVIDER_NAME);
+      }
+      default -> throw new NoSuchAlgorithmException("Unsupported digest type: " + digestType);
     }
+  }
+
+  /**
+   * Returns a boolean indicating whether the current hash function is fixed-size or not.
+   *
+   * @return true if the current hash function is fixed-size, false otherwise.
+   */
+  public static boolean isIsFixedHash() {
+    return isFixedHash;
   }
 }
