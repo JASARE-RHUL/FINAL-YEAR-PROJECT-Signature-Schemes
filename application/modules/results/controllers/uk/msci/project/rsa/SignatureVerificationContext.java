16398dc3e30005709f62aa0e21663e2fa9a85da7
Jude Asare <jude.asare.2020@live.rhul.ac.uk>
2024-03-07 14:57:42 +0000

    Refactor Signature Operation Contexts with New Abstract Base Class
    Introduce SignatureBaseContext as an abstract class, extending BenchmarkingContext. This new class serves as a foundational base for signature operation contexts, enhancing the architecture and providing a common interface for shared functionalities.
    
    Refactor SignatureCreationContext and SignatureVerificationContext to inherit from SignatureBaseContext. This change centralises common logic and properties, improving the code structure and maintainability.
    
    Implement SignatureCreationContext and SignatureVerificationContext as subclasses of SignatureBaseContext, each tailored to handle specific aspects of signature creation and verification. This clear hierarchy improves code readability and separation of concerns.
    
    Add method getSignatureResultsLabel(boolean isComparisonMode, String signatureOperationText) in SignatureBaseContext to generate context-specific result labels. This method is overridden in subclasses to cater to the nuances of creation and verification operations, providing flexibility in label generation based on operation type and mode (comparison or normal benchmarking).
    
    Maintain existing functionalities related to exporting batches, key configurations, hash functions, and provably secure status. Ensure that these functionalities seamlessly integrate with the new class structure.

--- application/modules/results/controllers/uk/msci/project/rsa/SignatureVerificationContext.java
+++ application/modules/results/controllers/uk/msci/project/rsa/SignatureVerificationContext.java
@@ -1,17 +1,13 @@
 package uk.msci.project.rsa;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
 
 /**
  * This class is a specialised context for the benchmarking of signature verification operations. It
  * extends BenchmarkingContext to provide functionality specific to signature verification, such as
  * exporting verification decisions to the generalised results view.
  */
-public class SignatureVerificationContext extends BenchmarkingContext {
-
-  private SignatureModel signatureModel;
+public class SignatureVerificationContext extends SignatureBaseContext {
 
   /**
    * Constructs a new SignatureVerificationContext with a given SignatureModel.
@@ -19,7 +15,7 @@ public class SignatureVerificationContext extends BenchmarkingContext {
    * @param signatureModel The SignatureModel associated with this context.
    */
   public SignatureVerificationContext(SignatureModel signatureModel) {
-    this.signatureModel = signatureModel;
+    super(signatureModel);
   }
 
   /**
@@ -47,113 +43,17 @@ public class SignatureVerificationContext extends BenchmarkingContext {
   }
 
   /**
-   * Provides a context-specific label for the results view based on the specific signature
-   * operation that was benchmarked. This label is used to display relevant information about the
-   * benchmarking context in the UI, offering users a clear understanding of the results being
-   * presented. The label can vary depending on whether the benchmarking is conducted in normal
-   * benchmarking mode displaying the single hash function used, differing from comparison mode
-   * where multiple hash functions can potentially be used so the hash function name is omitted from
-   * label.
+   * Overrides the method from the base class to provide a specific label for the signature
+   * verification operation results. Utilises the base method to construct the label based on the
+   * comparison mode status and sets the operation text to "Verification".
    *
-   * @param isComparisonMode A boolean flag indicating whether the benchmarking is done in
-   *                         comparison mode, which may affect the label content.
-   * @return A string label describing the benchmarking results for signature verification.
+   * @param isComparisonMode Indicates whether the operations were conducted in comparison mode.
+   * @return A descriptive label for the signature verification operation results.
    */
   @Override
-  public String getResultsLabel(boolean isComparisonMode) {
-    return isComparisonMode ? "Benchmarking Results for Signature Verification ("
-        + signatureModel.getSignatureType() + ")"
-        : "Benchmarking Results for Signature Verification ("
-            + signatureModel.getSignatureType() + "-" + signatureModel.getHashType() + ")";
+  public String getSignatureResultsLabel(boolean isComparisonMode) {
+    return getSignatureResultsLabel(isComparisonMode, "Verification");
   }
 
-  /**
-   * Retrieves a map linking each group index to its corresponding list of hash functions and their
-   * provable security status.
-   *
-   * @return A map where each key is a group index and each value is a list of pairs of {@code
-   * DigestType} and a boolean indicating if it's provably secure.
-   */
-  @Override
-  public Map<Integer, List<HashFunctionSelection>> getKeyConfigToHashFunctionsMap() {
-    return signatureModel.getKeyConfigToHashFunctionsMap();
-  }
-
-  /**
-   * Retrieves the number of keys considered as a group in the signature creation benchmarking
-   * context. This grouping is relevant for applying different hash functions and parameters during
-   * the benchmarking process.
-   *
-   * @return The number of keys per group used in the benchmarking.
-   */
-  public int getKeysPerGroup() {
-    return signatureModel.getKeysPerGroup();
-  }
-
-  /**
-   * Provides an array representing the number of trials performed for each key within a group in
-   * the context of signature verification benchmarking.
-   *
-   * @return An array where each element corresponds to the number of trials for a key in a specific
-   * group.
-   */
-  @Override
-  public int[] getTrialsPerKeyByGroup() {
-    return signatureModel.getTrialsPerKeyByGroup();
-  }
-
-  /**
-   * Retrieves the total number of hash functions used across all groups in the signature
-   * verification benchmarking.
-   *
-   * @return The total number of hash functions used in the benchmarking process.
-   */
-  @Override
-  public int getTotalHashFunctions() {
-    return signatureModel.getTotalHashFunctions();
-  }
-
-  /**
-   * Gets the total number of distinct groups formed for the purpose of signature creation
-   * benchmarking. Each group represents a unique combination of key configurations or hash
-   * functions.
-   *
-   * @return The total number of groups in the benchmarking process.
-   */
-  @Override
-  public int getTotalGroups() {
-    return signatureModel.getTotalGroups();
-  }
-
-  /**
-   * Retrieves the fraction used to calculate the custom hash size based on provided key lengths in
-   * normal benchmarking mode.
-   *
-   * @return corresponding fraction represented as an int list
-   */
-  @Override
-  public int[] getCustomHashSizeFraction() {
-    return signatureModel.getCustomHashSizeFraction();
-  }
-
-  /**
-   * Retrieves the type of hash function currently set in the model.
-   *
-   * @return The current hash function type.
-   */
-  @Override
-  public DigestType getHashType() {
-    return signatureModel.getHashType();
-  }
-
-  /**
-   * Indicates whether the signature scheme operates in provably secure mode.
-   *
-   * @return {@code true} if the signature scheme is operating in provably secure mode, {@code
-   * false} otherwise.
-   */
-  public boolean getProvablySecure() {
-    return signatureModel.getProvablySecure();
-  }
 }
 
