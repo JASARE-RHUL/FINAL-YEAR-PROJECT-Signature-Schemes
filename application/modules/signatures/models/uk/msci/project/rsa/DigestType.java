9f825792c1e9bdf91f5a9b1468671991f7918da4
Jude Asare <jude.asare.2020@live.rhul.ac.uk>
2024-03-07 15:20:55 +0000

    Chnage string representations of DigestType Enums to a human readable format

--- application/modules/signatures/models/uk/msci/project/rsa/DigestType.java
+++ application/modules/signatures/models/uk/msci/project/rsa/DigestType.java
@@ -11,38 +11,38 @@ public enum DigestType {
    * SHA-256 hash function. Provides a standard cryptographic hash function with a fixed output size
    * of 256 bits.
    */
-  SHA_256("SHA_256"),
+  SHA_256("SHA-256"),
 
   /**
    * SHA-512 hash function. Offers a cryptographic hash function with a fixed output size of 512
    * bits, providing a higher level of security than SHA-256.
    */
-  SHA_512("SHA_512"),
+  SHA_512("SHA-512"),
 
   /**
    * SHAKE-128 hash function. A member of the SHA-3 family of hash functions, capable of producing
    * variable-length output. SHAKE-128 is designed for flexibility and security.
    */
-  SHAKE_128("SHAKE_128"),
+  SHAKE_128("SHAKE-128"),
 
   /**
    * SHAKE-256 hash function. Similar to SHAKE-128, this hash function is part of the SHA-3 family
    * and can produce a variable-length output but with a higher security level than SHAKE-128.
    */
-  SHAKE_256("SHAKE_256"),
+  SHAKE_256("SHAKE-256"),
 
   /**
    * MGF1 with SHA-256 hash function. Represents a mask generation function using SHA-256. It's
    * often used in cryptographic protocols requiring data masking with a hash function like
    * SHA-256.
    */
-  MGF_1_SHA_256("MGF_1_SHA_256"),
+  MGF_1_SHA_256("SHA-256 with MGF1"),
 
   /**
    * MGF1 with SHA-512 hash function. Similar to MGF1 with SHA-256 but utilizes SHA-512 for
    * cryptographic operations requiring a higher security level.
    */
-  MGF_1_SHA_512("MGF_1_SHA_512");
+  MGF_1_SHA_512("SHA-512 with MGF1");
 
   /**
    * The name of the hash digest. This field stores a string representation of the hash function,
